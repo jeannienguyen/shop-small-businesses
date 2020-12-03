ceda22b554c6927c6a07b9c80e7efb57d0a86dcc
Jeannie Nguyen <tm.jeannie@gmail.com>
2020-12-03 09:58:06 -0800

    add helmet for seo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { Layout } from "components/common";
 import { Businesses, Menu } from "components/landing";
 import "assets/js/main.js";
 
 export default () => (
   <Layout>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Shop Small Businesses</title>
+      <link rel="canonical" href="https://shop-small-businesses.netlify.app/" />
+    </Helmet>
     <Menu />
     <Businesses />
   </Layout>

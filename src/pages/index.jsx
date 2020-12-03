853f650cdd57c27d07ea0b4a896c5ad8d6ddb3b3
Jeannie Nguyen <tm.jeannie@gmail.com>
2020-12-03 10:21:10 -0800

    update meta

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,6 +10,18 @@ export default () => (
     <Helmet>
       <meta charSet="utf-8" />
       <meta name="image" content={Thumbnail} />
+      <meta
+        property="og:url"
+        content={"https://shop-small-businesses.netlify.app/"}
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={"Shop Small Businesses"} />
+      <meta
+        property="og:description"
+        content={
+          "Shop small businesses this year - each order makes a world of difference."
+        }
+      />
       <meta property="og:image" content={Thumbnail} />
       <title>Shop Small Businesses</title>
       <link rel="canonical" href="https://shop-small-businesses.netlify.app/" />

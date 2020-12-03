e9ec863301feb406d615168a055810f8390aeecf
Jeannie Nguyen <tm.jeannie@gmail.com>
2020-12-03 10:12:28 -0800

    add thumbnail

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { Layout } from "components/common";
 import { Businesses, Menu } from "components/landing";
+import Thumbnail from "../assets/thumbnail/shop-small-businesses.png";
 import "assets/js/main.js";
 
 export default () => (
   <Layout>
     <Helmet>
       <meta charSet="utf-8" />
+      <meta name="image" content={Thumbnail} />
+      <meta property="og:image" content={Thumbnail} />
       <title>Shop Small Businesses</title>
       <link rel="canonical" href="https://shop-small-businesses.netlify.app/" />
     </Helmet>
